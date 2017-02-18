108b637614ded07a146650915c5f1894ad57ba10
konstantin8105 <konstantin8105@gmail.com>
2017-02-18 12:25:11 +0300

    backup ready to tests

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# file_manipulation_go
+# Package "file_manipulation_go"
 
 [![GoDoc](https://godoc.org/github.com/Konstantin8105/file_manipulation_go/fileManipulation?status.svg)](https://godoc.org/github.com/Konstantin8105/file_manipulation_go/fileManipulation)
+
+## Goals( *Цели* )
+
+1. Cleaing present folders from trash STAAD files.
+   *Отчистка папок от мусорных файлов, создаваемых программой STAAD*
+2. Create copy of file without STAAD trash files.
+   *Создание копий файлов без копирования мусорных файлоа STAAD*
+
+See file **main.go** like example
+*Смотрите файл **main.go** в качестве примера*
--- fileManipulation/backup.go
+++ fileManipulation/backup.go
@@ -2,6 +2,9 @@ package fileManipulation
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
 	"runtime"
 )
 
@@ -21,13 +24,14 @@ func BackUp(inputFolder, outputFolder Folder) error {
 		return fmt.Errorf("Input and output folder cannot be same")
 	}
 
+	// use all allowable proccesors
 	defer runtime.GOMAXPROCS(runtime.GOMAXPROCS(0))
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	inputFileFlow, errChannel := getInputFilesFlow(inputFolder)
+	// working
+	files, errChannel := getInputFilesFlow(inputFolder)
 	defer close(*errChannel)
-	copyFileFlow := filterInputFiles(inputFileFlow, outputFolder, errChannel)
-	success := copyFiles(copyFileFlow, errChannel)
+	success := copyFiles(files, inputFolder, outputFolder, errChannel)
 	defer close(success)
 
 	select {
@@ -43,28 +47,112 @@ func getInputFilesFlow(inputFolder Folder) (<-chan fileParam, *(chan error)) {
 	errFunc := make(chan error)
 	go func() {
 		defer close(inputFiles)
-		//folders := getInternalDirectory(inputFolder, &errFunc)
-		//for folder := range folders {
-		// TODO
-		//}
+		folders := getInternalDirectory(inputFolder, &errFunc)
+		for folder := range folders {
+			isStaadFolder, err := folder.withStaadFiles()
+			if err != nil {
+				errFunc <- err
+				return
+			}
+			files, err := ioutil.ReadDir(string(folder))
+			if err != nil {
+				errFunc <- err
+				return
+			}
+			f := folder
+			for _, file := range files {
+				if !file.IsDir() {
+					if (isStaadFolder && !isStaadTempFile(file.Name())) || !isStaadFolder {
+
+						inputFiles <- fileParam{fileInfo: file, path: f}
+					}
+				}
+			}
+		}
+
 	}()
 	return inputFiles, &errFunc
 }
 
-func filterInputFiles(inputFiles <-chan fileParam, outputFolder Folder, errChannel *(chan error)) <-chan fileParam {
-	copyFiles := make(chan fileParam)
-	go func() {
-		defer close(copyFiles)
-		// TODO
-	}()
-	return copyFiles
-}
-
-func copyFiles(copyFiles <-chan fileParam, errChannel *(chan error)) chan bool {
+// files come with format:
+// file.info .... Name = "kernel.dll" ...
+// file.path = "C://Windows//"
+// inputFolder = "C:"
+// outputFolder = "X:"
+// Transformations of file name:
+// inFileName  = C://Windows//kernel.dll
+// outFileName = X://Windows//kernel.dll
+func copyFiles(files <-chan fileParam, inputFolder Folder, outputFolder Folder, errChannel *(chan error)) chan bool {
 	success := make(chan bool)
 	go func() {
-		// TODO
+		for file := range files {
+			fmt.Println("Income file  = ", file)
+			fmt.Println("inputFolder  = ", inputFolder)
+			fmt.Println("outputFolder = ", outputFolder)
+
+			inFileName := fmt.Sprintf("%s\\%s", string(inputFolder), file.fileInfo.Name())
+			outFileName := fmt.Sprintf("%s\\%s\\%s", string(outputFolder), string(file.path)[(len(string(inputFolder))+1):], file.fileInfo.Name())
+
+			fmt.Println("inFile ======> ", inFileName)
+			fmt.Println("outFile =====> ", outFileName)
+
+			// optimization of copy time:
+			// - if files with same time, then no copy
+			// - if files with same size, then no copy
+			var copy bool
+			copy, err := isNeedCopy(inFileName, outFileName)
+			if err != nil {
+				*errChannel <- err
+				return
+			}
+
+			fmt.Println("isNeedCopy == ", copy)
+			fmt.Println("\n\n\n\n")
+			/*
+				if copy {
+					err := CopyWithCheckingMd5(inFileName, outFileName)
+					if err != nil {
+						*errChannel <- err
+						return
+					}
+				}
+			*/
+		}
 		success <- true
 	}()
 	return success
+}
+
+func isNeedCopy(in, out string) (b bool, err error) {
+	b = true
+	var inFileInfo, outFileInfo os.FileInfo
+	// check out file is exist, if not exist, then need copy
+	if outFileInfo, err = os.Stat(out); os.IsNotExist(err) {
+		// out does not exist
+		return b, nil
+	}
+	// check in file is exist, if not exist, then error
+	if inFileInfo, err = os.Stat(out); os.IsNotExist(err) {
+		// out does not exist
+		return b, err
+	}
+
+	// if diff time or size, then need copy
+	if inFileInfo.Size() != outFileInfo.Size() {
+		return b, nil
+	}
+	if inFileInfo.ModTime() != outFileInfo.ModTime() {
+		return b, nil
+	}
+
+	// in 0.1% cases just copy //
+	var (
+		amount      = 1000
+		luckyNumber = 222
+	)
+	if rand.Intn(amount) == luckyNumber {
+		return b, nil
+	}
+
+	return false, nil
 }
